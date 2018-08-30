78c40746773295b39b7596d5b4091603e7732e24
ndc07 <ndc07.it@gmail.com>
2018-08-30 10:12:01 +0700

    fixed menu-button for table/mobile

--- js/index.js
+++ js/index.js
@@ -1,31 +1,38 @@
 function showNavigation() {
     var ul = document.querySelector('#nav-left ul');
-    if (ul.style.display === 'none') {
+    var icon = document.querySelector('a#menu-button i');
+    if (ul.style.display !== 'block') {
+        icon.className = 'fa fa-close';
         // ul
         ul.style.display = 'block';
         ul.style.position = 'absolute';
         ul.style.top = '9%';
         ul.style.zIndex = '2';
-        ul.style.minWidth = '140px';
+        ul.style.width = '100%';
         ul.style.backgroundColor = '#3A2D32';
         //li
-        var _li = document.querySelectorAll('header div nav div#nav-left ul > li');
-        _li.forEach(function(li) {
-            li.style.display = 'block';
-            li.style.padding = '10px 22px';
+        var li = document.querySelectorAll('header div nav div#nav-left ul > li');
+        li.forEach(function(item) {
+            item.style.display = 'block';
+            item.style.padding = '4px 22px 0 22px';
         });
         // li#home
         var home = document.querySelector('#nav-left ul li#home');
-        home.style.paddingTop = '12px';
-        home.style.paddingBottom = '12px';
+        home.style.paddingTop = '4px';
+        home.style.paddingBottom = '4px';
         //other
         document.querySelector('#nav-left ul li:first-child').style.margin = '0';
-        document.querySelector('#nav-left ul li:last-child').style.paddingBottom = '20px';
-        // dropdown-content
-        var content = document.querySelector('#nav-left ul li.dropdown .dropdown-content');
-        content.style.left = '100%';
-        content.top = '-15%';
+        document.querySelector('#nav-left ul li:last-child').style.paddingBottom = '4px';
+        
+        //customize dropdown-content
+        var content = document.querySelectorAll('#nav-left ul li.dropdown .dropdown-content');
+        content.forEach(function(item) {
+            item.style.left = '100%';
+            item.style.top = '-15%';
+        });
+        
     } else {
+        icon.className = 'fa fa-navicon';
         ul.style.display = 'none';
     }
 }
